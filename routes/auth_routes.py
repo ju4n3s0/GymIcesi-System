8ccb2f926610e63c07f5fb0fb08036c174b6f251
ju4n3s0 <juanestebansotelomera@gmail.com>
2025-11-06 21:34:48 -0500

    Reestructuración

--- routes/auth_routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-
-auth_bp = Blueprint("auth", __name__)
-
-@auth_bp.route("/ping", methods=["GET"])
-def ping():
-    return jsonify({"message": "Gym Icesi backend funcionando correctamente "})
